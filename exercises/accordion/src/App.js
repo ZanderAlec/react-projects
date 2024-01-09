c82a43114579780adbb32fba67925d2f4395761f
José Alexandre da Silva França <alexandrefranca208@gmail.com>
2024-01-09 11:17:21 -0300

    Refatoração components

--- exercises/accordion/src/App.js
+++ exercises/accordion/src/App.js
@@ -3,19 +3,16 @@ import './App.css';
 
 const questions = [ 
   {
-    id: "01",
     title: "Where are these chairs assembled?",
     answer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
   },
 
   {
-    id: "02",
     title: "How long do i have to return my chair?",
     answer: "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
   },
 
   {
-    id: "03",
     title: "Do you ship to countries outside the EU?",
     answer: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. "
   }
@@ -24,23 +21,32 @@ const questions = [
 function App() {
 
   return (
-    <div className="container">
-        {questions.map((quest) => {
-          return <Question question = {quest} />
-        })}
+    <div >
+       <Accordeon data = { questions }/>
     </div>
   );
 }
 
-const Question = ({question}) =>{
+//Recebe uma lista (data) como prop, de forma que pode ser reutilizado para diferentes listas.
+const Accordeon = ({ data }) => {
+  return (
+  <div className="container">
+    {data.map((quest, index) => {
+      return <Question question = {quest} index = {index} key = {quest.title} />
+    })}
+  </div>
+  );
+}
+
+const Question = ({index, question}) =>{
 
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className={`quest-container ${ isOpen && 'quest-open' }`} >
-      <div class="flex">
+      <div className="flex">
           <div className={ `${isOpen && 'green'} flex`}>
-            <h2 className = "quest-num" > { question.id }</h2>
+            <h2 className = "quest-num" > { index < 9 ? `0${index+1}` : index+1 }</h2>
             <h2 className="quest-title"> { question.title } </h2> 
           </div>
           
