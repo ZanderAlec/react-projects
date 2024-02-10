f752a23c1d832dda1ca295cdd037bdbaffd8f6a8
José Alexandre da Silva França <alexandrefranca208@gmail.com>
2024-02-10 15:40:48 -0300

    current buttons e responsividade

--- exercises/carrossel/src/App.js
+++ exercises/carrossel/src/App.js
@@ -1,101 +1,107 @@
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 
 function App() {
   return (
     <div >
-        <Carrossel >
-          <CarrosselContent>
-            <div className="bg-red-700	 w-full h-full">
-              <div>
-                asdasdas
-              </div>
-            </div>
-          </CarrosselContent>
-
-          <CarrosselContent>
-            <div className="bg-blue-700	 w-full h-auto			">
+      <Carrossel >
+        <CarrosselContent>
+          <div className="bg-red-700	 w-full h-full">
             <div>
-                lorem saiduygasuydgausygd uyasgduygasuyd uasgdu yagsuydgasuygduyasguydgasuygduya sgduygasuydg
-              </div>
+              asdasdas
             </div>
-          </CarrosselContent>
+          </div>
+        </CarrosselContent>
 
-
-          <CarrosselContent>
-            <div className="bg-green-700	 h-auto			">
-              <div>
-                lorem saiduygasuydgausygd uyasgduygasuyd asdasdsadsadasd yagsuydgasuygduyasguydgasuygduya sgduygasuydg uyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyd
-              </div>asd
+        <CarrosselContent>
+          <div className="bg-blue-700	 w-full h-auto			">
+            <div>
+              lorem saiduygasuydgausygd uyasgduygasuyd uasgdu yagsuydgasuygduyasguydgasuygduya sgduygasuydg
             </div>
-          </CarrosselContent>
+          </div>
+        </CarrosselContent>
 
-        </Carrossel>
+
+        <CarrosselContent>
+          <div className="bg-green-700	 h-auto			">
+            <div>
+              lorem saiduygasuydgausygd uyasgduygasuyd asdasdsadsadasd yagsuydgasuygduyasguydgasuygduya sgduygasuydg uyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyd
+            </div>asd
+          </div>
+        </CarrosselContent>
+
+        <CarrosselContent>
+          <div className="bg-green-700	 h-auto			">
+            <div>
+              lorem saiduygasuydgausygd uyasgduygasuyd asdasdsadsadasd yagsuydgasuygduyasguydgasuygduya sgduygasuydg uyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyduyasgduygasuyd
+            </div>asd
+          </div>
+        </CarrosselContent>
+
+      </Carrossel>
     </div>
   );
 }
 
-const Carrossel = ({children}) => {
-  const [curItem, setCurItem ] = useState(0);
+const Carrossel = ({ children }) => {
+  const [curItem, setCurItem] = useState(0);
 
-  function nextItem(){
-    curItem+1 == children.length ? setCurItem(0) :  setCurItem(curItem+1);
+  function nextItem() {
+    curItem + 1 === children.length ? setCurItem(0) : setCurItem(curItem + 1);
   }
 
-  function previousItem(){
-    curItem-1 < 0 ? setCurItem(children.length-1) :  setCurItem(curItem-1);
+  function previousItem() {
+    curItem - 1 < 0 ? setCurItem(children.length - 1) : setCurItem(curItem - 1);
+  }
+
+  const bttms = [];
+
+  for (let i = 0; i < children.length; i++) {
+    curItem === i ? bttms.push(<CurrentCarrosselBttm id={i} setCurItem = {setCurItem} active={true} />) 
+    : bttms.push(<CurrentCarrosselBttm id={i} setCurItem = {setCurItem}/>)
   }
 
 
-  return <div className = " border-2  border-solid border-black flex space-x-8 h-fit">
-    
-    <CarrosselBttm content = "<" clickFunction = {previousItem}/>
+  return <div className=" border-2  border-solid border-black flex content-between h-fit ">
+
+    <CarrosselBttm content="<" clickFunction={previousItem} />
 
     <div className='flex-1'>
       <div className="relative h-fit border-2 border-solid border-red-600  " >
         {children[curItem]}
       </div>
 
-
-      <CurrentBttmsContainer quant = {children.length} current = {curItem}/>
+      <div className='mt-4	 ml-auto	mr-auto flex space-x-1.5 w-fit	'>
+        {bttms}
+      </div>
     </div>
-  
 
-    <CarrosselBttm content = ">" clickFunction = {nextItem}/>
+    <CarrosselBttm content=">" clickFunction={nextItem} />
   </div>
 }
 
-const CarrosselContent = ({children}) => {
-  return <div className=" max-w-full h-max break-words">
-    {children}    
+const CarrosselContent = ({ children }) => {
+  return <div className="  h-max break-all">
+    {children}
   </div>
 }
 
-const CarrosselBttm = ({content, clickFunction}) => {
-  return <button 
-    className = " bg-white	rounded-full	px-4	py-3	border-2 border-solid border-black	content-center hover:bg-slate-300	 max-h-12			leading-5		" type=""
+const CarrosselBttm = ({ content, clickFunction }) => {
+  return <button
+    className=" bg-white	rounded-full	px-4	py-3	border-2 border-solid border-black	content-center hover:bg-slate-300	 max-h-12	leading-5	self-center	" type=""
     onClick={() => clickFunction()}
   >
     {content}
   </button>
 }
 
-const CurrentBttmsContainer = ({quant, current}) => {
 
-  const bttms = []
-
-  for(let i = 0; i < quant; i++){
-    current == i ? bttms.push(<CurrentCarrosselBttm active = {true} />) : bttms.push(<CurrentCarrosselBttm/>)
-  }
-
-  return <div className='mt-4	 ml-auto	mr-auto flex space-x-1.5 w-fit	'>
-    {bttms}
-  </div>
-}
-
-const CurrentCarrosselBttm = ({active = false}) => {
-  return <div className={`rounded-full ${active? "bg-gray-500" : "bg-gray-300"}	w-4	h-4	`}>
-  </div>
+const CurrentCarrosselBttm = ({id, setCurItem, active = false }) => {
+  return <button 
+  className={`rounded-full ${active ? "bg-gray-500" : "bg-gray-300"}	w-4	h-4	`}
+  onClick={() => setCurItem(id)}
+  >
+  </button>
 }
 
 export default App;
