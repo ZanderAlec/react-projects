cdd4e5703623c78d670ccce58541db5ad4b7a96c
José Alexandre da Silva França <alexandrefranca208@gmail.com>
2024-08-16 20:34:03 -0300

    next/previous buttons

--- exercises/multi-step-form/src/components/Form.js
+++ exercises/multi-step-form/src/components/Form.js
@@ -3,10 +3,18 @@ import StepList from './StepList'
 
 export default function Form() {
 
+  const numSteps = 3;
   const [currStep, setCurrStep] = useState(0);
+  function nextStep(){
+    setCurrStep(currStep + 1);
+  }
 
+  function previousStep(){
+    if(currStep-1 >= 0){
+      setCurrStep(currStep - 1);
+    }
+  }
   
-
   return (
     <div>
 
@@ -16,7 +24,14 @@ export default function Form() {
             <input />
         </form>
 
-       
+
+        <button disabled = {currStep === 0} onClick={() => previousStep()}>Previous</button>
+        { currStep !== numSteps - 1 ? 
+          <button onClick={() => nextStep()}>Next</button>
+          : <button > Finish</button>
+        }
+      
+
     </div>
   )
 }
